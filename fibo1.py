22984f1477848d15fe5cd3915c9f8551c42af193
Deven-14 <deven.paramaj@gmail.com>
2021-08-12 23:22:53 +0530

    fibo1 commit 1

---
+++ fibo1.py
@@ -0,0 +1,24 @@
+
+def input_n():
+    return int(input("Enter the number of Fibonacci numbers to be displayed: "))
+
+def fibonacci(n):
+    if n < 0:
+        return None
+    
+    yield 0
+    yield 1
+    num1, num2, next_num = 0, 1, 1
+    for _ in range(2, n):
+        next_num = num1 + num2
+        yield next_num
+        num1, num2 = num2, next_num
+
+def main():
+    n = input_n()
+    x = fibonacci(n)
+    for i in x:
+        print(i)
+
+if __name__ == "__main__":
+    main()
