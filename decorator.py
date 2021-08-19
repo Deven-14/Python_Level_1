ecd891352c1bd5f623e880439c7f7701b3a2b073
Deven-14 <deven.paramaj@gmail.com>
2021-08-19 13:27:26 +0530

    first commit

---
+++ decorator.py
@@ -0,0 +1,24 @@
+from functools import wraps
+
+def decorator(func):
+    @wraps(func)
+    def print_args_value(*args, **kwargs):
+        for ele in args:
+            print(ele)
+        for ele in kwargs.values():
+            print(ele)
+        x = func(*args, **kwargs)
+        return x
+
+    return print_args_value
+        
+
+@decorator
+def decorate(*args, **kwargs):
+    return "Completed"
+
+def main():
+    print(decorate(1, 2, c=3, d=4))
+
+if __name__ == "__main__":
+    main()
